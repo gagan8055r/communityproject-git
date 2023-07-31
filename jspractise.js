002814272ea20ae436031bca50b09cae926794da
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-08-01 00:43:17 +0530

    Create jspractise.js

---
+++ jspractise.js
@@ -0,0 +1,89 @@
+// 6 datatypes   
+// 1.undefined // 2.boolean// 3.number// 4.string// 5.bigint// 6.symbol
+
+// 10 -'5' is a bug ans=5
+// null is a object
+
+// console.log("agga"-"dfgh");  ans is NaN it is property of  a global object
+
+// console.log(NaN===NaN);  ans is false
+// console.log(isNaN(NaN));  True 
+// console.log(Number.isNaN(NaN)); True 
+
+// var a=15
+// var b=9
+
+// var b=a+b
+
+// // // var b=a+b
+// // var a=b-a
+// // console.log(a);    without using temp variable
+// // var b=b-a
+// // console.log(b);
+
+// // console.log(5=='5');
+// // console.log(5==='5');
+
+// var year=2015
+
+// // if(year%100==0)
+// // {
+//     //   console.log("not a  leap year");
+//     // }else
+//     // {
+//         //     if(year%400==0)
+//         //     {
+//             //         console.log("it is a leap year");
+//             //     }
+//             //     else
+//     //     {
+//         //         console.log("not a leap year");
+//         //     }
+//         // }
+        
+//         if(year%4==0)
+//         {
+//         if(year%100==0)
+//     {
+//       if(year%400==0)
+//       {
+//         console.log("it is leap year");
+//       }
+//       else
+//       {
+//         console.log("not a leap year");
+//       }
+//     }
+//     else
+//     {
+//         console.log("it is a leap year");
+//     }
+// }
+// else
+// {
+//     console.log("not a leap year");
+// }
+
+// console.log(b);
+
+// console.log(a>=b);
+// console.log(a==b);  false  for one assignment it is 19
+
+// function sum(a,b)
+// {
+//     console.log( a+b)
+// }
+// sum(10,30)
+// function sum(a,b)
+// {
+//     console.log( a+b)
+// }
+// var funExp=sum(10,30)
+// var funexp=function (a,b)            /////Anonymous function is always used with function expressions 
+// {
+//     return a+b
+// }
+
+// console.log(funexp(1,30));
+
+

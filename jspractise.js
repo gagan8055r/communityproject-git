50e15e6931543d130060d8e75652ce5ee6afd707
gagan8055r <123857923+gagan8055r@users.noreply.github.com>
2023-08-02 12:28:17 +0530

    Update jspractise.js

--- jspractise.js
+++ jspractise.js
@@ -15,11 +15,11 @@
 
 // var b=a+b
 
-// // // var b=a+b
-// // var a=b-a
-// // console.log(a);    without using temp variable
-// // var b=b-a
-// // console.log(b);
+// // var b=a+b
+// var a=b-a
+// console.log(a);    without using temp variable
+// var b=b-a
+// console.log(b);
 
 // // console.log(5=='5');
 // // console.log(5==='5');
@@ -86,4 +86,133 @@
 
 // console.log(funexp(1,30));
 
+// var fruits=["mango","jackfruit","lemon","banana","orange","mango"]
 
+// for(i in fruits)
+// {
+//     console.log(i);
+// }
+// for(i of fruits)
+// {
+//     console.log(i);
+// }
+
+// fruits.forEach((ele,array,index)=>{
+//     console.log(ele+" "+array+" "+index);
+// })
+
+// console.log(fruits.indexOf("mango",1));  if not found returns -1
+// console.log(fruits.lastIndexOf("mango",4));  if not found returns -1
+// console.log(fruits.includes("mango")); if not found returns -1
+
+
+// const find=numbers.find((element,array,index)=>{         
+    //     return element<0
+    //     // console.log("next");                 if we console in the function itself we get boolean values 
+    //     // console.log(element>5000);           or if we return the values we get this      find return undefined for false values
+    // })
+    // console.log(find);
+    // const findElm=numbers.findIndex((element)=>{         
+        //    return element<4
+        // })
+        
+        // console.log(findElm);
+        
+        
+        // const filter=numbers.filter((element,index)=>{
+            //     // return element<450                               ////for results it gives new array       for no results it gives empty array
+            //     return element>450                                                       
+            // })
+            
+            // console.log(filter);
+            
+            
+            
+            // sort method is used only in the string
+            // var names=["gag","dfghj","rtyu","sdfgh","sdfgh"]
+            // console.log(numbers.sort());
+            // console.log(names.sort());                                        push returns length of the pushed array
+            // const client=names.push("dfghj")                             
+            // const client=names.unshift("dfghj")                           pop returns which value is poped out  
+            // const client1=names.shift()                                      
+            // // const client1=names.pop()                             
+            // console.log(names);
+            // console.log(client1);
+            
+            // var names=["nikil","march","vimal","bvc","liks"]
+            
+            // const index=names.indexOf("vimal")
+            // // const changeName=names.splice(index,1,"March")
+            // const changeName=names.splice(index,1)
+            // console.log(names);
+            // console.log(changeName);
+            
+            // var numbers=[50,12,445,785,987,529]  
+            // //    does not mutate the original array
+            // const sort=numbers.map((ele,index,array)=>{
+            //     return ele>500
+            // })
+            // const sort1=numbers.forEach((ele,index,array)=>{
+            //     return ele>500
+            // })
+
+            // console.log(sort); 
+            // console.log(sort1); 
+            
+            // var arr=[2,3,4,6,8]
+
+            // let mul=arr.map((ele)=>{
+            //         return ele*2
+            // }).filter((elem)=>{
+            //     return elem >10
+            // }).reduce((accumulator,eleme)=>{
+            //     return accumulator+=eleme
+            // })
+
+            // console.log(mul);
+            // console.log(9-'5');
+            // console.log("sdfg"-"fghj");
+            // console.log(true+true);
+// const useless=null
+// console.log(typeof(useless));
+// const num=6
+// console.log(typeof(num));
+            // console.log(5=='5');
+            // console.log(5==='5');
+
+            // console.log(`${num} the is `);
+            // console.log('the anser of twp numbers is + 'num);
+
+            // var animals=["gaga",'fghj','hjkhj','fghjk']
+//             const names=animals.forEach((ele,index)=>{
+// console.log(ele+" "+index);
+//             })
+
+            // console.log(names);
+
+            // console.log(animals.indexOf("gaga",1));
+            // console.log(animals.indexOf("gaga"));
+            // console.log(animals.lastindexOf("gaga"));
+            // console.log(animals.lastIndexOf("gaga",2));
+
+
+
+            // var prices=[100,200,250,300,400,500]
+
+            // prices.find((ele)=>{
+            //     // return ele>250
+            //     console.log(ele>250);
+            // })
+
+//           const num=  prices.filter((ele)=>{
+//                 return ele>250
+//             })
+// console.log(num);
+
+let places="mysore,bdvt,bangalore,bang"
+// console.log(places.slice(0,1));
+// let res=places.slice(7,-4)
+// let res=places.search("bdvt")
+// let res=places.substring(0,5)
+// let res=places.replace("mysore","bangalore")           it will returns a new string//////
+console.log(res);
